7a1668d05561920730ca0b5434e496fb4b825b35
prasdud <prasdudwastaken@gmail.com>
2025-09-13 22:59:05 +0530

    fix(urls.py): edit main urls.py to work with feedback app

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('user_accounts.urls')),  # <-- this maps /auth/register  
+    path('auth/', include('user_accounts.urls')),
+    path('api/', include('feedback.urls')),
 ]

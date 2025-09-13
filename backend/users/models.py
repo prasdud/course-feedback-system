f4db378c2de1b2f1a77f36d48797c9841674f1bf
prasdud <prasdudwastaken@gmail.com>
2025-09-13 19:21:38 +0530

    feat(models): created basic user model, need to implement bcrypt

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,12 +1,17 @@
-from django.db import models
 import mongoengine as me
 # Create your models here.
 
 
 
-class User(models.model):
+class User(me.Document):
     name = me.StringField(required = True, max_length=100)
     email = me.EmailField(required = True, unique=True)
     password = me.StringField(required = True, min_length=8)
-    ROLE_CHOICES = ('admin', 'student')
+    ROLE_CHOICES = [('admin', 'admin'), ('student', 'student')]
     role = me.StringField(required = True, choices=ROLE_CHOICES, default='student')
+
+
+    #mongoDB collection name
+    meta = {
+        'collection': 'users'
+    }

93bc6955d52e7eb92444cc8dd6571106f663c797
prasdud <prasdudwastaken@gmail.com>
2025-09-14 21:09:55 +0530

    fix(deploy): added allowed_host for render

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -18,15 +18,37 @@ from django.urls import path, include
 import os
 from django.views.static import serve
 from django.conf import settings
+from django.http import FileResponse
+from django.conf.urls.static import static
 
-def serve_frontend(request, filename='index.html'):
-    path_to_file = os.path.join(settings.FRONTEND_DIR, filename)
-    return serve(request, os.path.basename(path_to_file), os.path.dirname(path_to_file))
 
+def serve_frontend_file(request, file_path=''):
+    """Serve frontend files directly"""
+    if not file_path:
+        file_path = 'index.html'  # Default to index.html
+    
+    frontend_dir = os.path.join(settings.BASE_DIR, '..', 'frontend')
+    file_full_path = os.path.join(frontend_dir, file_path)
+    
+    if os.path.exists(file_full_path) and os.path.isfile(file_full_path):
+        return FileResponse(open(file_full_path, 'rb'))
+    else:
+        # If file not found, serve index.html for SPA routing
+        index_path = os.path.join(frontend_dir, 'index.html')
+        if os.path.exists(index_path):
+            return FileResponse(open(index_path, 'rb'))
+    
+    from django.http import Http404
+    raise Http404("File not found")
 
 urlpatterns = [
-    path('', serve_frontend, name='index'),
     path('admin/', admin.site.urls),
     path('auth/', include('user_accounts.urls')),
     path('api/', include('feedback.urls')),
+    path('<path:file_path>', serve_frontend_file, name='serve_frontend'),
+    path('', serve_frontend_file, name='home'),
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
